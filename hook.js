14896a8de7fa9eee38d3d73b6643ff3a921fbb64
Guy Kaplan <gkpln3@gmail.com>
2022-02-18 22:16:50 +0200

    Made everything super automatic

--- hook.js
+++ hook.js
@@ -1,17 +1,16 @@
-   
+
 console.log("Script loaded successfully ");
 Java.perform(function x() { //Silently fails without the sleep from the python code
     console.log("Inside java perform function");
     //get a wrapper for our class
-    var my_class = Java.use("e.c.h.b");
+    var my_class = Java.use("e.c.g.a.b");
     //replace the original implmenetation of the function `fun` with our custom function
-    my_class.a.implementation = function (a1, a2, a3, a4) {
+    my_class.a.implementation = function (a1) {
         //print the original arguments
         // console.log("original call: fun(" + x + ", " + y + ")");
         //call the original implementation of `fun` with args (2,5)
-
-        var ret_value = this.a(a1, a2, a3, a4);
-		send(ret_value);
+        var ret_value = this.a(a1);
+		send({'name': a1, 'data': ret_value});
         return ret_value;
     }
 });
--- loader.py
+++ loader.py
@@ -1,24 +1,98 @@
 import time
-
+import os
 import frida
+import tempfile
+import glob
+import shutil
+import queue
+import termcolor
 
-device = frida.get_usb_device()
-pid = device.spawn(["com.yit.evritViewer"])
-device.resume(pid)
-time.sleep(1)  # Without it Java.perform silently fails
-session = device.attach(pid)
-with open("hook.js") as f:
-    script = session.create_script(f.read())
+tempdir = tempfile.mkdtemp()
+decrypted_queue = queue.Queue()
+def start_listening_for_decryptions():
+	global tempdir
+	device = frida.get_usb_device()
+	pid = device.spawn(["com.yit.evritViewer"])
+	device.resume(pid)
+	time.sleep(1)  # Without it Java.perform silently fails
+	session = device.attach(pid)
+	with open("hook.js") as f:
+		script = session.create_script(f.read())
 
-count = 3
-def handle_message(message, data):
-	global count
-	with open(f"book/chapter-{count}.xhtml", "w") as f:
-		f.write(message['payload'])
-	count+=1
-	# print(message['payload'])
-script.on("message", handle_message)
-script.load()
+	def handle_message(message, data):
+		global tempdir
+		filename: str = message['payload']['name'][0]
+		filename = filename[filename.find("com_yit_evrit"):]
+
+		decrypted_queue.put({'name': filename, 'data': message['payload']['data']})
+	script.on("message", handle_message)
+	script.load()
+
+def download_encrypted_epubs() -> list:
+	global tempdir
+	print(f"Downloading epubs to {tempdir}")
+	os.system(f"adb pull /data/data/com.yit.evritViewer/files/Books/ {tempdir}")
+	epubs = []
+	for epub in glob.glob(f"{tempdir}/Books/*.epub"):
+		print(f"Unpacking {epub}")
+		extract_dir = epub.strip(".epub")
+		shutil.unpack_archive(epub, format="zip", extract_dir=extract_dir)
+
+		# Add extracted epub to the list of extracted epubs.
+		epubs.append(extract_dir)
+
+	return epubs
+
+def get_encrypted_files(epub):
+	def is_binary(f):
+		return b'\x00' in open(f, 'rb').read()
+
+	files = glob.glob(f"{epub}/OEBPS/*.xhtml")
+	return [f for f in files if is_binary(f)]
+
+
+def pack_finished_books():
+	global epub_files
+
+	for epub in list(epub_files.keys()):
+		if len(epub_files[epub]) == 0:
+			print(termcolor.colored(f"Got all files for {epub.split('/')[-1]}, repacking...", "green"))
+			shutil.make_archive(f"{os.path.basename(epub)}", "zip", root_dir=epub)
+			shutil.move(f"{os.path.basename(epub)}.zip", f"{os.path.basename(epub)}.epub")
+			del epub_files[epub]
+			print(termcolor.colored(f"Done", "green"))
+	return len(epub_files) == 0
 
 # prevent the python script from terminating
-input()
+epubs = download_encrypted_epubs()
+
+# Get a list of files to decrypt for each epub.
+epub_files = {}
+for epub in epubs:
+	epub_files[epub] = get_encrypted_files(epub)
+
+
+start_listening_for_decryptions()
+
+while not pack_finished_books():
+	try:
+		decrypted_file = decrypted_queue.get()
+		print(decrypted_file['name'], end="")
+		decrypted_epub = decrypted_file['name'].split("/")[0]
+
+		# Find relevant epub.
+		epub = [e for e in epub_files.keys() if decrypted_epub in e][0]
+		current_epub_files: list = epub_files[epub]
+		current_file: str = [f for f in current_epub_files if decrypted_file['name'].split("/")[-1] in f][0]
+
+		with open(current_file, "w") as f:
+			f.write(decrypted_file['data'])
+		current_epub_files.remove(current_file)
+		epub_files[epub] = current_epub_files
+		print(f" {len(current_epub_files)} remaining...")
+		
+	except IndexError as e:
+		print()
+		pass
+
+print("All epubs finished")
