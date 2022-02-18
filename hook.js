6b48e3fda775a75cc485abfaae430825e64b713e
gkpln3 <gkpln3@gmail.com>
2022-02-18 22:35:45 +0200

    Removed redundant comments.

--- hook.js
+++ hook.js
@@ -1,16 +1,11 @@
 
 console.log("Script loaded successfully ");
-Java.perform(function x() { //Silently fails without the sleep from the python code
+Java.perform(function x() { // Silently fails without the sleep from the python code
     console.log("Inside java perform function");
-    //get a wrapper for our class
     var my_class = Java.use("e.c.g.a.b");
-    //replace the original implmenetation of the function `fun` with our custom function
     my_class.a.implementation = function (a1) {
-        //print the original arguments
-        // console.log("original call: fun(" + x + ", " + y + ")");
-        //call the original implementation of `fun` with args (2,5)
         var ret_value = this.a(a1);
-		send({'name': a1, 'data': ret_value});
+	send({'name': a1, 'data': ret_value});
         return ret_value;
     }
 });
